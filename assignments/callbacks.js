2c90b6ad7bbb6c78d25e3b00968be2d708b93e52
larynab <fsovreign@gmail.com>
2019-01-22 14:52:00 -0800

    array-methods

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -33,11 +33,19 @@ getLength(items, function() {
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(items[items.length - 1]);
 }
+last(items, function(last){
+  console.log(last)
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+sumNums(3, 5, function(sum) {
+  console.log(sum)
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
